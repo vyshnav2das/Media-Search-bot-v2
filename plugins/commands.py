1a96ce2ba5459bb4347aa635f0e2b0780ce6145e
Jinn <85754819+Jinn-of-telegram@users.noreply.github.com>
2021-09-29 09:16:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,6 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 from utils import Media, get_file_details
 from Database import Database
+from plugins.Broadcast import broadcast
 
 from pyrogram.errors import UserNotParticipant
 logger = logging.getLogger(__name__)
