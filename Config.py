e02fb4d830401e9876df3bd511d7eec56635fd4f
Jinn <85754819+Jinn-of-telegram@users.noreply.github.com>
2021-09-29 08:22:20 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -27,7 +27,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 # MongoDB information
 DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['BOT_NAME']
+DATABASE_NAME = environ['DATABASE_NAME']
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
